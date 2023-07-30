4e7d73b927df84fb2c2ae57f21a37f587c5dbebe
zilian <zilian@DESKTOP-5NR6F56>
2023-07-30 17:17:49 +0800

    Add project files.

---
+++ ADOConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+        // Display title as the C# console calculator app.
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
+
+        while (!endApp)
+        {
+            // Declare variables and set to empty.
+            string numInput1 = "";
+            string numInput2 = "";
+            double result = 0;
+
+            // Ask the user to type the first number.
+            Console.Write("Type a number, and then press Enter: ");
+            numInput1 = Console.ReadLine();
+
+            double cleanNum1 = 0;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.Write("This is not valid input. Please enter an integer value: ");
+                numInput1 = Console.ReadLine();
+            }
+
+            Console.WriteLine("------------------------\n");
+
+            // Wait for the user to respond before closing.
+            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            if (Console.ReadLine() == "n") endApp = true;
+
+            Console.WriteLine("\n"); // Friendly linespacing.
+        }
+        return;
+    }
+}
